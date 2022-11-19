18cea8af557d305c5f6a4953f6ff772a99c7601e
Gman1906Sibs <77187437+Gman1906Sibs@users.noreply.github.com>
2022-11-19 20:13:25 +0200

    dash edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,7 @@ const Header = () => {
   return (
     <header className="main-header navbar">
       <div className="col-search">
-        <form className="searchform">
+        {/*<form className="searchform">
           <div className="input-group">
             <input
               list="search_terms"
@@ -51,7 +51,7 @@ const Header = () => {
             <option value="Apple iphone" />
             <option value="Ahmed Hassan" />
           </datalist>
-        </form>
+        </form>*/}
       </div>
       <div className="col-nav">
         <button
